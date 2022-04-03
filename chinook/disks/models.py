67875aacc17d53ba7d2498e128e2272e11295e03
Tanguy Pérennou <Tanguy.Perennou@isae-supaero.fr>
2022-04-03 19:35:44 +0200

    Add models classes for Chinook
    Found unobvious field types here:
    
    - https://docs.djangoproject.com/fr/4.0/ref/models/fields/#model-field-types
    
    Current choice for unitPrice: FloatField, although DecimalField would
    also work
    
    More references for model writing:
    
    - https://docs.djangoproject.com/fr/4.0/intro/tutorial02
    - https://docs.djangoproject.com/fr/4.0/topics/db/models
    - https://docs.djangoproject.com/fr/4.0/topics/db/models/#field-types

--- chinook/disks/models.py
+++ chinook/disks/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Track(models.Model):
+    """
+    A single track in an album
+    """
+    name = models.CharField(max_length=200)
+    composer = models.CharField(max_length=200)
+    milliseconds = models.IntegerField('Duration (ms)')
+    bytes = models.IntegerField('Size (bytes)')
+    unitPrice = models.FloatField('Unit Price (EUR)') 
+    # unitPrice: a DecimalField(max_digits=3, decimal_places=2) would also work
+    album = models.ForeignKey('Album', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Album(models.Model):
+    """
+    An album, by a single artist, with several tracks
+    """
+    title = models.CharField(max_length=200)
+    artist = models.ForeignKey('Artist', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Artist(models.Model):
+    """
+    An artist, that may have many albums
+    """
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
