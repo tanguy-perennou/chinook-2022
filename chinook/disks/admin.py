f953efccaa77335c835e58b606cbade1f863db70
Tanguy Pérennou <Tanguy.Perennou@isae-supaero.fr>
2022-04-03 19:40:54 +0200

    Add admin pages for Chinook models
    Also had to run `manage.py createsuperuser` to create an admin account.
    After that:
    
    1. launch the server with `manage.py runserver`
    2. browse http://127.0.0.1:8000/admin
    3. log in with admin account
    4. browse all chinook db content

--- chinook/disks/admin.py
+++ chinook/disks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Track, Album, Artist
+
+# Register disks models
+admin.site.register(Track)
+admin.site.register(Album)
+admin.site.register(Artist)
