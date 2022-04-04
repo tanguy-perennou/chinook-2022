71a131d104cbdedede390605260fcd352cf3e2fa
Tanguy Pérennou <Tanguy.Perennou@isae-supaero.fr>
2022-04-04 12:11:00 +0200

    Add db access and templates-based rendering to views
    Minimalist views, ugly. Functional, though: db content is shown,
    and navigation between albums and back to list is possible.
    
    Reference:
    
    - https://docs.djangoproject.com/fr/4.0/intro/tutorial03/

--- chinook/disks/views.py
+++ chinook/disks/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+
+from .models import Album
 
 
 def albums_list(request):
-    return HttpResponse("List of albums here soon.")
+    albums = get_list_or_404(Album)
+    return render(request, 'disks/albums_list.html', {'albums': albums})
 
 
 def album_details(request, album_id):
-    return HttpResponse("You're looking at album %s." % album_id)
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'disks/album_details.html', {'album': album})
