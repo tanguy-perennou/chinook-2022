2940754125d0a0b0217fffa78cac0b66c0564dca
Tanguy Pérennou <Tanguy.Perennou@isae-supaero.fr>
2022-04-13 12:37:38 +0200

    Use bootstrap styles in research form
    Have to use widgets in the form class to style the input field.
    For the label field, to add specific styling, see comment in
    the template; you will have to use a verbose {% for ... %} loop.
    
    References:
    - https://docs.djangoproject.com/fr/4.0/topics/forms/#working-with-form-templates
    - https://docs.djangoproject.com/fr/4.0/topics/forms/#more-on-fields
    - https://getbootstrap.com/docs/5.0/forms/overview/
    - https://medium.com/swlh/how-to-style-your-django-forms-7e8463aae4fa

--- chinook/disks/forms.py
+++ chinook/disks/forms.py
@@ -4,4 +4,7 @@ class SearchForm(forms.Form):
     """
     Simplest possible form, just a text box for a query
     """
-    query = forms.CharField(label='Search in album titles', max_length=100, required=False)
+    query = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'e.g. all', 'class': 'form-control'}),
+        label='Search in album titles', max_length=100, required=False
+        )
